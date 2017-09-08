edc01b135611bee67033baa9a36b2de8992c02ef
duanchao <chaoduan1993@gmail.com>
2017-09-08 18:06:05 +0800

    函数(上)

---
+++ day_05/Function.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+函数
+@version: 1.0
+@author: duanchao
+@license: Apache Licence 
+@file: Function.py
+@time: 2017/9/8 14:40
+"""
+
+def sayhello():
+    "hello world"
+    return "hello world"
+
+
+
+print sayhello()
+
+###  在python中, 类型属于对象, 变量是没有类型的
+
+
+
+
+
+
